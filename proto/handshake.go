e00a275b9a2132337c7a5e104b07609ff82fe841
Péter Szilágyi <peterke@gmail.com>
2013-03-19 21:18:11 +0100

    Refactoring

--- proto/handshake.go
+++ proto/handshake.go
@@ -19,18 +19,12 @@
 package proto
 
 import (
-	"crypto/cipher"
-	"crypto/hkdf"
-	"crypto/hmac"
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/sts"
-	"fmt"
-	"hash"
-	"io"
 	"log"
-	"proto/stream"
 	"math/big"
+	"proto/stream"
 )
 
 // Authenticated connection request message. Contains the originators ID for
@@ -203,41 +197,4 @@ func authenticate(strm *stream.Stream, key *rsa.PrivateKey, store map[string]*rs
 		return
 	}
 	sink <- newSession(strm, secret)
-}
-
-// Creates a new session from the given data stream and negotiated secret. The
-// derived cryptographic primitives are configured in the config.go file. Any
-// failure here means invalid/corrupt configurations, thus will lead to a panic.
-func newSession(strm *stream.Stream, secret []byte) *Session {
-	// Create the key derivation function
-	hasher := func() hash.Hash { return hkdfHash.New() }
-	hkdf := hkdf.New(hasher, secret, hkdfSalt, hkdfInfo)
-
-	// Extract the symmetric key and create the block cipher
-	key := make([]byte, sesCipherBits/8)
-	n, err := io.ReadFull(hkdf, key)
-	if n != len(key) || err != nil {
-		panic(fmt.Sprintf("Failed to extract session key: %v", err))
-	}
-	block, err := sesCipher(key)
-	if err != nil {
-		panic(fmt.Sprintf("Failed to create session cipher: %v", err))
-	}
-	// Extract the IV for the counter mode and create the stream cipher
-	iv := make([]byte, block.BlockSize())
-	n, err = io.ReadFull(hkdf, iv)
-	if n != len(iv) || err != nil {
-		panic(fmt.Sprintf("Failed to extract session IV: %v", err))
-	}
-	stream := cipher.NewCTR(block, iv)
-
-	// Extract the HMAC key and create the session MACer
-	salt := make([]byte, sesHash().Size())
-	n, err = io.ReadFull(hkdf, salt)
-	if n != len(salt) || err != nil {
-		panic(fmt.Sprintf("Failed to extract session mac salt: %v", err))
-	}
-	mac := hmac.New(sesHash, salt)
-
-	return &Session{*strm, stream, mac}
 }
--- proto/session.go
+++ proto/session.go
@@ -20,13 +20,54 @@ package proto
 
 import (
 	"crypto/cipher"
+	"crypto/hkdf"
+	"crypto/hmac"
+	"fmt"
 	"hash"
+	"io"
 	"proto/stream"
 )
 
+// Accomplishes secure and authenticated communication.
 type Session struct {
-	stream.Stream
-
+	socket *stream.Stream
 	cipher cipher.Stream
 	mac    hash.Hash
+}
+
+// Creates a new session from the given data stream and negotiated secret. The
+// derived cryptographic primitives are configured in the config.go file. Any
+// failure here means invalid/corrupt configurations, thus will lead to a panic.
+func newSession(strm *stream.Stream, secret []byte) *Session {
+	// Create the key derivation function
+	hasher := func() hash.Hash { return hkdfHash.New() }
+	hkdf := hkdf.New(hasher, secret, hkdfSalt, hkdfInfo)
+
+	// Extract the symmetric key and create the block cipher
+	key := make([]byte, sesCipherBits/8)
+	n, err := io.ReadFull(hkdf, key)
+	if n != len(key) || err != nil {
+		panic(fmt.Sprintf("Failed to extract session key: %v", err))
+	}
+	block, err := sesCipher(key)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to create session cipher: %v", err))
+	}
+	// Extract the IV for the counter mode and create the stream cipher
+	iv := make([]byte, block.BlockSize())
+	n, err = io.ReadFull(hkdf, iv)
+	if n != len(iv) || err != nil {
+		panic(fmt.Sprintf("Failed to extract session IV: %v", err))
+	}
+	stream := cipher.NewCTR(block, iv)
+
+	// Extract the HMAC key and create the session MACer
+	salt := make([]byte, sesHash().Size())
+	n, err = io.ReadFull(hkdf, salt)
+	if n != len(salt) || err != nil {
+		panic(fmt.Sprintf("Failed to extract session mac salt: %v", err))
+	}
+	mac := hmac.New(sesHash, salt)
+
+	return &Session{strm, stream, mac}
 }
