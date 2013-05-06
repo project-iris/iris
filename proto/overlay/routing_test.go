d89c91b7e6418ee64d4d579b8d6f03b0ee5ba1b7
Péter Szilágyi <peterke@gmail.com>
2013-05-06 18:11:54 +0200

    Smoothed out tests a bit + detailed benchmarks.

--- proto/overlay/routing_test.go
+++ proto/overlay/routing_test.go
@@ -20,6 +20,7 @@
 package overlay
 
 import (
+	"bytes"
 	"config"
 	"crypto/rand"
 	"crypto/x509"
@@ -69,22 +70,83 @@ func TestRouting(t *testing.T) {
 	// Wait a while for the handshakes to complete
 	time.Sleep(3 * time.Second)
 
+	// Create the messages to pass around
+	meta := []byte{0x99, 0x98, 0x97, 0x96}
+	head := session.Header{make([]byte, len(meta)), []byte{0x00, 0x01}, []byte{0x02, 0x03}, nil}
+	copy(head.Meta, meta)
+
+	msgs := make([][]session.Message, peers)
+	for i := 0; i < peers; i++ {
+		msgs[i] = make([]session.Message, peers)
+		for j := 0; j < peers; j++ {
+			msgs[i][j].Head = head
+			msgs[i][j].Data = []byte(nodes[i].nodeId.String() + nodes[j].nodeId.String())
+		}
+	}
 	// Check that each node can route to everybody
-	for _, src := range nodes {
-		for _, dst := range nodes {
-			src.Send(dst.nodeId, new(session.Message))
+	for i, src := range nodes {
+		for j, dst := range nodes {
+			src.Send(dst.nodeId, &msgs[i][j])
+			time.Sleep(250 * time.Millisecond) // Makes the deliver order verifyable
 		}
 	}
 	// Sleep a bit and verify
 	time.Sleep(time.Second)
 	for i := 0; i < peers; i++ {
 		if len(apps[i].delivs) != peers {
-			t.Errorf("app #%v: message count mismatch: %v.", i, apps[i].delivs)
+			t.Errorf("app #%v: message count mismatch: have %v, want %v.", i, len(apps[i].delivs), peers)
+		} else {
+			for j := 0; j < peers; j++ {
+				// Check contents (a bit reduced, not every field was verified below)
+				if bytes.Compare(meta, apps[j].delivs[i].Head.Meta) != 0 {
+					t.Errorf("send/receive meta mismatch: have %v, want %v.", apps[i].delivs[j].Head.Meta, meta)
+				}
+				if bytes.Compare(msgs[i][j].Data, apps[j].delivs[i].Data) != 0 {
+					t.Errorf("send/receive data mismatch: have %v, want %v.", apps[i].delivs[j].Data, msgs[j][i].Data)
+				}
+			}
 		}
 	}
 }
 
-type repeater struct {
+func BenchmarkPassing1Byte(b *testing.B) {
+	benchmarkPassing(b, 1)
+}
+
+func BenchmarkPassing16Byte(b *testing.B) {
+	benchmarkPassing(b, 16)
+}
+
+func BenchmarkPassing256Byte(b *testing.B) {
+	benchmarkPassing(b, 256)
+}
+
+func BenchmarkPassing1KByte(b *testing.B) {
+	benchmarkPassing(b, 1024)
+}
+
+func BenchmarkPassing4KByte(b *testing.B) {
+	benchmarkPassing(b, 4096)
+}
+
+func BenchmarkPassing16KByte(b *testing.B) {
+	benchmarkPassing(b, 16384)
+}
+
+func BenchmarkPassing64KByte(b *testing.B) {
+	benchmarkPassing(b, 65536)
+}
+
+func BenchmarkPassing256KByte(b *testing.B) {
+	benchmarkPassing(b, 262144)
+}
+
+func BenchmarkPassing1MByte(b *testing.B) {
+	benchmarkPassing(b, 1048576)
+}
+
+// Overlay callback app which will send one message at a time, waiting for delivery
+type sequencer struct {
 	over *Overlay
 	dest *big.Int
 	msgs []session.Message
@@ -92,44 +154,43 @@ type repeater struct {
 	quit chan struct{}
 }
 
-func (r *repeater) Deliver(msg *session.Message) {
-	if r.left--; r.left < 0 {
-		close(r.quit)
+func (s *sequencer) Deliver(msg *session.Message) {
+	if s.left--; s.left < 0 {
+		close(s.quit)
 	} else {
-		r.over.Send(r.dest, &r.msgs[r.left])
+		s.over.Send(s.dest, &s.msgs[s.left])
 	}
 }
 
-func BenchmarkPassing(b *testing.B) {
+func benchmarkPassing(b *testing.B, block int) {
+	b.SetBytes(int64(block))
+	key, _ := x509.ParsePKCS1PrivateKey(privKeyDer)
+
 	// Make sure all previously started tests and benchmarks terminate fully
 	time.Sleep(time.Second)
 
-	key, _ := x509.ParsePKCS1PrivateKey(privKeyDer)
-
-	// Generate a bach of messages to send around
+	// Generate a batch of messages to send around
 	head := session.Header{[]byte{0x99, 0x98, 0x97, 0x96}, []byte{0x00, 0x01}, []byte{0x02, 0x03}, nil}
-
-	block := 8192
-	b.SetBytes(int64(block))
 	msgs := make([]session.Message, b.N)
 	for i := 0; i < b.N; i++ {
 		msgs[i].Head = head
 		msgs[i].Data = make([]byte, block)
 		io.ReadFull(rand.Reader, msgs[i].Data)
 	}
-	// Create two overlay nodes to communicate
+	// Create the sender node
 	send := New(appId, key, nil)
 	send.Boot()
 	defer send.Shutdown()
 
-	recvApp := &repeater{send, nil, msgs, b.N, make(chan struct{})}
+	// Create the receiver app to sequence messages and the associated overlay node
+	recvApp := &sequencer{send, nil, msgs, b.N, make(chan struct{})}
 	recv := New(appId, key, recvApp)
 	recvApp.dest = recv.nodeId
 	recv.Boot()
 	defer recv.Shutdown()
 
 	// Wait a while for booting to finish
-	time.Sleep(time.Second)
+	time.Sleep(2 * time.Second)
 
 	// Reset timer and start message passing
 	b.ResetTimer()
@@ -137,6 +198,43 @@ func BenchmarkPassing(b *testing.B) {
 	<-recvApp.quit
 }
 
+func BenchmarkThroughput1Byte(b *testing.B) {
+	benchmarkThroughput(b, 1)
+}
+
+func BenchmarkThroughput16Byte(b *testing.B) {
+	benchmarkThroughput(b, 16)
+}
+
+func BenchmarkThroughput256Byte(b *testing.B) {
+	benchmarkThroughput(b, 256)
+}
+
+func BenchmarkThroughput1KByte(b *testing.B) {
+	benchmarkThroughput(b, 1024)
+}
+
+func BenchmarkThroughput4KByte(b *testing.B) {
+	benchmarkThroughput(b, 4096)
+}
+
+func BenchmarkThroughput16KByte(b *testing.B) {
+	benchmarkThroughput(b, 16384)
+}
+
+func BenchmarkThroughput64KByte(b *testing.B) {
+	benchmarkThroughput(b, 65536)
+}
+
+func BenchmarkThroughput256KByte(b *testing.B) {
+	benchmarkThroughput(b, 262144)
+}
+
+func BenchmarkThroughput1MByte(b *testing.B) {
+	benchmarkThroughput(b, 1048576)
+}
+
+// Overlay pllication callback to wait for a number of messages and signal afterwards.
 type waiter struct {
 	left int
 	quit chan struct{}
@@ -148,17 +246,15 @@ func (w *waiter) Deliver(msg *session.Message) {
 	}
 }
 
-func BenchmarkThroughput(b *testing.B) {
+func benchmarkThroughput(b *testing.B, block int) {
+	b.SetBytes(int64(block))
+	key, _ := x509.ParsePKCS1PrivateKey(privKeyDer)
+
 	// Make sure all previously started tests and benchmarks terminate fully
 	time.Sleep(time.Second)
 
-	key, _ := x509.ParsePKCS1PrivateKey(privKeyDer)
-
 	// Generate a bach of messages to send around
 	head := session.Header{[]byte{0x99, 0x98, 0x97, 0x96}, []byte{0x00, 0x01}, []byte{0x02, 0x03}, nil}
-
-	block := 8192
-	b.SetBytes(int64(block))
 	msgs := make([]session.Message, b.N)
 	for i := 0; i < b.N; i++ {
 		msgs[i].Head = head
@@ -182,7 +278,7 @@ func BenchmarkThroughput(b *testing.B) {
 		}
 	}
 	// Wait a while for booting to finish
-	time.Sleep(time.Second)
+	time.Sleep(2 * time.Second)
 
 	// Reset timer and start message passing
 	b.ResetTimer()
