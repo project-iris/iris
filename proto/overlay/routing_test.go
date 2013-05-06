11a7cc694902aca71a8a6f96cc2a0eeabddb146a
Péter Szilágyi <peterke@gmail.com>
2013-05-06 17:10:31 +0200

    Initial (very rough) routing benchmarks.

--- proto/overlay/routing_test.go
+++ proto/overlay/routing_test.go
@@ -21,7 +21,10 @@ package overlay
 
 import (
 	"config"
+	"crypto/rand"
 	"crypto/x509"
+	"io"
+	"math/big"
 	"proto/session"
 	"testing"
 	"time"
@@ -79,4 +82,110 @@ func TestRouting(t *testing.T) {
 			t.Errorf("app #%v: message count mismatch: %v.", i, apps[i].delivs)
 		}
 	}
+}
+
+type repeater struct {
+	over *Overlay
+	dest *big.Int
+	msgs []session.Message
+	left int
+	quit chan struct{}
+}
+
+func (r *repeater) Deliver(msg *session.Message) {
+	if r.left--; r.left < 0 {
+		close(r.quit)
+	} else {
+		r.over.Send(r.dest, &r.msgs[r.left])
+	}
+}
+
+func BenchmarkPassing(b *testing.B) {
+	// Make sure all previously started tests and benchmarks terminate fully
+	time.Sleep(time.Second)
+
+	key, _ := x509.ParsePKCS1PrivateKey(privKeyDer)
+
+	// Generate a bach of messages to send around
+	head := session.Header{[]byte{0x99, 0x98, 0x97, 0x96}, []byte{0x00, 0x01}, []byte{0x02, 0x03}, nil}
+
+	block := 8192
+	b.SetBytes(int64(block))
+	msgs := make([]session.Message, b.N)
+	for i := 0; i < b.N; i++ {
+		msgs[i].Head = head
+		msgs[i].Data = make([]byte, block)
+		io.ReadFull(rand.Reader, msgs[i].Data)
+	}
+	// Create two overlay nodes to communicate
+	send := New(appId, key, nil)
+	send.Boot()
+	defer send.Shutdown()
+
+	recvApp := &repeater{send, nil, msgs, b.N, make(chan struct{})}
+	recv := New(appId, key, recvApp)
+	recvApp.dest = recv.nodeId
+	recv.Boot()
+	defer recv.Shutdown()
+
+	// Wait a while for booting to finish
+	time.Sleep(time.Second)
+
+	// Reset timer and start message passing
+	b.ResetTimer()
+	recvApp.Deliver(nil)
+	<-recvApp.quit
+}
+
+type waiter struct {
+	left int
+	quit chan struct{}
+}
+
+func (w *waiter) Deliver(msg *session.Message) {
+	if w.left--; w.left <= 0 {
+		close(w.quit)
+	}
+}
+
+func BenchmarkThroughput(b *testing.B) {
+	// Make sure all previously started tests and benchmarks terminate fully
+	time.Sleep(time.Second)
+
+	key, _ := x509.ParsePKCS1PrivateKey(privKeyDer)
+
+	// Generate a bach of messages to send around
+	head := session.Header{[]byte{0x99, 0x98, 0x97, 0x96}, []byte{0x00, 0x01}, []byte{0x02, 0x03}, nil}
+
+	block := 8192
+	b.SetBytes(int64(block))
+	msgs := make([]session.Message, b.N)
+	for i := 0; i < b.N; i++ {
+		msgs[i].Head = head
+		msgs[i].Data = make([]byte, block)
+		io.ReadFull(rand.Reader, msgs[i].Data)
+	}
+	// Create two overlay nodes to communicate
+	send := New(appId, key, nil)
+	send.Boot()
+	defer send.Shutdown()
+
+	wait := &waiter{b.N, make(chan struct{})}
+	recv := New(appId, key, wait)
+	recv.Boot()
+	defer recv.Shutdown()
+
+	// Create the sender to push the messages
+	sender := func() {
+		for i := 0; i < len(msgs); i++ {
+			send.Send(recv.nodeId, &msgs[i])
+		}
+	}
+	// Wait a while for booting to finish
+	time.Sleep(time.Second)
+
+	// Reset timer and start message passing
+	b.ResetTimer()
+	go sender()
+	<-wait.quit
 }
