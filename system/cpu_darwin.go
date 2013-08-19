ba61f47594ac97a4eabf549a5122f0bee0d21055
Péter Szilágyi <peterke@gmail.com>
2013-08-19 16:48:29 +0300

    Update Iris summary line.

--- system/cpu_darwin.go
+++ system/cpu_darwin.go
@@ -1,3 +1,24 @@
+// Iris - Decentralized Messaging Framework
+// Copyright 2013 Peter Szilagyi. All rights reserved.
+//
+// Iris is dual licensed: you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// The framework is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+// more details.
+//
+// Alternatively, the Iris framework may be used in accordance with the terms
+// and conditions contained in a signed written agreement between you and the
+// author(s).
+//
+// Author: peterke@gmail.com (Peter Szilagyi)
+
+// This file contains the CPU measurements for Mac OS X.
+
 package system
 
 /*
