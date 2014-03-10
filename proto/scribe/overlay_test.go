6c0695c8a1d52bdc00c6212baa6d915ca711442b
Péter Szilágyi <peterke@gmail.com>
2014-03-10 21:05:54 +0200

    Scribe direct messaging test.

--- proto/scribe/overlay_test.go
+++ proto/scribe/overlay_test.go
@@ -26,9 +26,8 @@ import (
 	"testing"
 	"time"
 
-	"github.com/karalabe/iris/proto"
-
 	"github.com/karalabe/iris/config"
+	"github.com/karalabe/iris/proto"
 )
 
 type collector struct {
@@ -254,6 +253,102 @@ func TestBalance(t *testing.T) {
 				t.Fatalf("failed to unsubscribe from topic: %v.", err)
 			}
 			time.Sleep(time.Second)
+		}
+		// Terminate the node
+		if err := live[i].Shutdown(); err != nil {
+			t.Fatalf("failed to terminate scribe node: %v.", err)
+		}
+		time.Sleep(time.Second)
+	}
+}
+
+// Tests whether direct addressing works.
+func TestDirect(t *testing.T) {
+	// Override the overlay configuration
+	swapConfigs()
+	defer swapConfigs()
+
+	nodes := 10
+	msgs := 100
+
+	// Make sure there are enough ports to use
+	olds := config.BootPorts
+	defer func() { config.BootPorts = olds }()
+
+	for i := 0; i < nodes; i++ {
+		config.BootPorts = append(config.BootPorts, 65500+i)
+	}
+	// Load the private key and start a single scribe node
+	key, _ := x509.ParsePKCS1PrivateKey(privKeyDer)
+
+	// Boot an origin node to receive the direct messages
+	coll := &collector{
+		publish: []*proto.Message{},
+		balance: []*proto.Message{},
+		direct:  []*proto.Message{},
+	}
+	origin := New(overId, key, coll)
+	if _, err := origin.Boot(); err != nil {
+		t.Fatalf("failed to boot origin node: %v.", err)
+	}
+	defer func() {
+		if err := origin.Shutdown(); err != nil {
+			t.Fatalf("failed to terminate origin node: %v.", err)
+		}
+	}()
+
+	// Gradually start up scribe nodes, and send a direct message with every second
+	live := make([]*Overlay, 0, nodes)
+	for i := 0; i < nodes; i++ {
+		// Start the node
+		node := New(overId, key, coll)
+		live = append(live, node)
+
+		if _, err := node.Boot(); err != nil {
+			t.Fatalf("failed to boot scribe node: %v.", err)
+		}
+		time.Sleep(time.Second)
+
+		// If it's a messenger, send direct packets
+		if i%2 == 0 {
+			for j := 0; j < msgs; j++ {
+				msg := &proto.Message{
+					Data: []byte{byte(i)},
+				}
+				if err := node.Direct(origin.pastry.Self(), msg); err != nil {
+					t.Fatalf("failed to send direct message: %v.", err)
+				}
+			}
+			// Wait a while and check event counts
+			time.Sleep(time.Second)
+			if n := len(coll.direct); n != msgs {
+				t.Fatalf("arrive event mismatch: have %v, want %v", n, msgs)
+			} else {
+				// Reset the event collector
+				coll.direct = coll.direct[:0]
+			}
+		}
+	}
+	// Execute the inverse of the previous sequence, gradually terminating the nodes
+	for i := nodes - 1; i >= 0; i-- {
+		// If it's a messenger, send direct packets
+		if i%2 == 0 {
+			for j := 0; j < msgs; j++ {
+				msg := &proto.Message{
+					Data: []byte{byte(i)},
+				}
+				if err := live[i].Direct(origin.pastry.Self(), msg); err != nil {
+					t.Fatalf("failed to send direct message: %v.", err)
+				}
+			}
+			// Wait a while and check event counts
+			time.Sleep(time.Second)
+			if n := len(coll.direct); n != msgs {
+				t.Fatalf("arrive event mismatch: have %v, want %v", n, msgs)
+			} else {
+				// Reset the event collector
+				coll.direct = coll.direct[:0]
+			}
 		}
 		// Terminate the node
 		if err := live[i].Shutdown(); err != nil {
